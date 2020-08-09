32145be43d82e741ddfe28fb313f024418557435
Victor Savoi <savoi.victor@hotmail.com>
2020-08-08 21:40:24 -0300

    Login and Register response with more details

--- src/VSDev.Api/Controllers/AuthController.cs
+++ src/VSDev.Api/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -73,7 +74,7 @@ namespace VSDev.Api.Controllers
             return CustomResponse();
         }
 
-        private async Task<string> GerarJWT(string email)
+        private async Task<LoginResponseViewModel> GerarJWT(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
@@ -106,7 +107,19 @@ namespace VSDev.Api.Controllers
 
             var encodedToken = tokenHandler.WriteToken(token);
 
-            return encodedToken;
+            var response = new LoginResponseViewModel
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoEmHoras).TotalSeconds,
+                User = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return response;
         }
         private static long ToUnixEpochDate(DateTime date)
             => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
---
+++ src/VSDev.Api/DTOs/LoginResponseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace VSDev.Api.DTOs
+{
+
+    public class LoginResponseViewModel
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserTokenViewModel User { get; set; }
+    }
+
+    public class UserTokenViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<ClaimViewModel> Claims { get; set; }
+    }
+
+    public class ClaimViewModel
+    {
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+
+}
